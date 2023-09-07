dcc5c897e324a8c95c93e74fc7a2e750c165ac56
joao_arnold.py <joaopaulo167@gmail.com>
2023-09-07 03:15:28 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by João Paulo Arnold Barros on 07/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
